d577236efc584aad2d9faf392e98a3d0bdd4e122
Alex Gjorvad <alexgjorvad@gmail.com>
2018-05-10 10:46:48 -0500

    added post and get for movies in movies services file

---
+++ server/public/services/movies.services.js
@@ -0,0 +1,36 @@
+app.service('MoviesService', function ($http) {
+    console.log('MoviesService is loaded');
+    var self = this;
+
+self.newProperty= {name: '', genre: '', releaseDate: '', runTime: ''};
+self.addMovie = function () {
+    console.log( 'button click is working');
+    $http({
+        method: 'POST',
+        url: '/movie',
+        data: self.newMovie
+    })
+    .then(function (response) {
+        console.log('POST TO /movie successful');
+    })
+    .catch(function (error) {
+        console.log('POST to /movie unsuccessful', error);
+    })
+    self.getAllMovies();
+}
+
+self.getAllMovies = function() {
+    $http({
+        method: 'GET',
+        url: '/genre'
+    })
+    .then(function (response) {
+        console.log(response);
+        self.movie.list = response.data;
+    })
+    .catch(function (error) {
+        console.log('error on /genre GET', error);
+    });
+    }
+    self.getAllMovies();
+});
